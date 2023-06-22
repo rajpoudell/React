72a10413cc03cf78587b2d2169de5b17ab5e9ac8
Raj Poudel <raj.poudeel@gmail.com>
2023-06-22 14:05:24 +0545

    images file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+
 function App() {
+const [num,setNum] = useState(0);
+console.log("it render outside the useeffect")
+
+useEffect(() =>{
+  alert("useeffect is called")
+})
+  
   return (
     <div className="App">
-
+      <div>
+        <button onClick={(e) =>{setNum(num+1)}}>clicked {num}</button>
+      </div>
     </div>
   );
 }
